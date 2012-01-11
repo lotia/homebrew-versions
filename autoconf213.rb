fc1696250f921fa12cfcdc1998244ab0dcad465b
Paul O’Shannessy <paul@oshannessy.com>
2012-01-11 10:53:02 -0800

    autoconf213: remove keg-only limitation
    Don't make autoconf213 keg only. Since it's being built with a version
    suffix as autoconf213, there shouldn't be any conflicts.
    
    Closes #142.
    
    [jn: remove unnecessary gawk dep, style things]
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- autoconf213.rb
+++ autoconf213.rb
@@ -1,23 +1,17 @@
 require 'formula'
 
 class Autoconf213 < Formula
-  url 'http://ftpmirror.gnu.org/autoconf/autoconf-2.13.tar.gz'
   homepage 'http://www.gnu.org/software/autoconf/'
+  url 'http://ftpmirror.gnu.org/autoconf/autoconf-2.13.tar.gz'
   md5 '9de56d4a161a723228220b0f425dc711'
 
-  depends_on 'gawk'
-
-  keg_only <<-EOS.undent
-    autoconf-2.13 is required to build mozilla source.
-    In order to prevent conflicts with OSX or homebrew supplied autoconf, we
-    are defaulting this installation to keg-only. For info on building Mozilla:
-      http://developer.mozilla.org/en/build_documentation
-  EOS
-
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--program-suffix=213",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--infodir=#{info}",
+                          "--datadir=#{share}/autoconf213"
     system "make install"
   end
 end
